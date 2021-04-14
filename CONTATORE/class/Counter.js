90ffe14d0ef76331ec93239cb5f17f1f1950d3eb
Mitro09 <mpatrick94@libero.it>
2021-04-14 19:38:58 +0200

    tutto fatto tranne stringhe e non so perchè

--- CONTATORE/class/Counter.js
+++ CONTATORE/class/Counter.js
@@ -1,21 +1,17 @@
 class Counter {
     
     constructor(value=0,min=null,max=null){
-        if(max != null && value>=max){
-            this.value=max;
-        }
-        else if(min != null && value<=min){
-            this.value=min;
-        }
-        else{
-            this.value = value;
-        }
-        
-        this.max = max;
-        //console.log("MAX",this.max)
-        this.min = min;
-    }
 
+        this.diventaInt(value,min,max);
+        //this.value = parseInt(value);
+        
+        this.checkMax(value,max);
+        this.checkMin(value,min);
+        
+        this.max = (max);
+        //console.log("MAX",this.max)
+        this.min = (min);
+    }
 
     increment(){
         if(this.max == null){
@@ -33,6 +29,36 @@ class Counter {
         }
         else if(this.value>this.min){
             this.value--;
+        }
+    }
+
+    checkMax(value,max){
+        if(max != null && max<value){
+            this.value = (max);
+            //throw new Error("initial è maggiore di max")
+        }
+        else{
+            this.value = (value);
+        }
+    }
+
+    checkMin(value,min){
+        if(min != null && min>value){
+            this.value = (min)
+            throw new Error("initial è minore di min")
+        }
+        else{
+            this.value = (value);
+        }
+    }
+
+    diventaInt(value,min,max){
+        value = parseInt(value);
+        if(min != null){
+            min = parseInt(min);
+        }
+        if(max != null){
+            max = parseInt(max)
         }
     }
 }
